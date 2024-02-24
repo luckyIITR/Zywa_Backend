25250a7eb7eb551dcb08c4449f8dd97052b575b8
Lucky Kushwaha <luckykushwaha56@gmail.com>
2024-02-25 02:33:42 +0530

    updated

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from routes import card_route
+from starlette import status
 
 
 load_dotenv()
 app = FastAPI()
 
 app.include_router(card_route.router)
+
+@app.get("/", status_code=status.HTTP_200_OK)
+def welcome():
+    return {"msg" : "Welcome to Zywa's Backend Engineer Assignment. Go to /docs to get all details"}
